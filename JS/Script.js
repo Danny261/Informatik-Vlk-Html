ef05f1178b8e9f341a392d7a28e7636fb476a9e8
Danny <sodawerda@gmail.com>
2024-01-14 14:38:43 +0100

    finale

--- JS/Script.js
+++ JS/Script.js
@@ -1,6 +1,6 @@
-function showAlert() {
-    window.alert("Button wurde geklickt!");
-}
-{
-document.getElementById("buttonMessage").addEventListener("click", showAlert);
-}
+//function showAlert() {
+    //window.alert("Button wurde geklickt!");
+//}
+//{
+//document.getElementById("buttonMessage").addEventListener("click", showAlert);
+//}
--- JS/script3.js
+++ JS/script3.js
@@ -1,12 +1,4 @@
 function submitForm() {
-    // ausgefülltes löschen
-    document.getElementById('contactForm').reset();
-
-    // Nachricht wird anzeigen
     var messageContainer = document.getElementById('messageContainer');
     messageContainer.textContent = 'Nachricht gesendet';
-    setTimeout(function ()
-    {
-        messageContainer.textContent = '';
-    }, 3000); // ncachricht nach 3 Sekunden ausblenden
 }
